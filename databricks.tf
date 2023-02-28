9ae29958ceaa45c116dc9b9873c8d20d6d88beb0
ngkazuretraining6 <ngkazuretraining6@gmail.com>
2023-02-28 13:27:29 +0530

    added the dbricks provider

--- databricks.tf
+++ databricks.tf
@@ -1,7 +1,7 @@
 
 
 resource "azurerm_databricks_workspace" "dbs1" {
-  name                = "ngkdatabrickstest"
+  name                = "ngkdatabrickstest1"
   resource_group_name = azurerm_resource_group.githubactions.name
   location            = azurerm_resource_group.githubactions.location
   sku                 = "standard"
