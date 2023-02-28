f5bc40272c3e97d8a7405c42b33c4f5a2049d48b
ngkazuretraining6 <ngkazuretraining6@gmail.com>
2023-02-28 13:16:33 +0530

    added the dbricks provider

--- main.tf
+++ main.tf
@@ -7,18 +7,21 @@ terraform {
     key                  = "2w2eNcrzE/Djf+4OGo6mJ6LDCGnzrojZ3GKAzMoB1rxomGehCr3Dg5QHJMY8AF6uyeO5WNaHsy35+AStY2cDwA=="
   }
   */
+  
+  required_providers {
+    databricks = {
+      source = "databrickslabs/databricks"
+    }
+  }
 }
- 
+
 provider "azurerm" {
   # The "feature" block is required for AzureRM provider 2.x.
   # If you're using version 1.x, the "features" block is not allowed.
   #version = "~>2.0"
   features {}
 }
-databricks = {
-      source = "databrickslabs/databricks"
-    }
- 
+
 #Create Resource Group
 resource "azurerm_resource_group" "githubactions" {
   name     = "privatedbs2"
