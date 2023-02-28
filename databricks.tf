8165f3ad29c4c87a3f9e84d055c2417a08c26229
Krishna, Neerati G <gopala.krishna@optum.com>
2023-02-28 18:48:11 +0000

    added the workspace conf

--- databricks.tf
+++ databricks.tf
@@ -12,7 +12,6 @@ resource "azurerm_databricks_workspace" "dbs1" {
 }
 provider "databricks" {
   azure_workspace_resource_id = azurerm_databricks_workspace.dbs1.id
-  token = "dapie8932256a3d02ac4b88d087375031c7d-3"
 }
 resource "databricks_workspace_conf" "new" {
   custom_config = {
