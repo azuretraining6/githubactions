67a7da65fd42fff6b4f93494d349747d96afd2da
Krishna, Neerati G <gopala.krishna@optum.com>
2023-03-01 18:23:54 +0000

    added role

---
+++ role.tf
@@ -0,0 +1,18 @@
+data "azurerm_resource_group" "primary" {
+    name = "privatedbs21"
+}
+
+resource "azurerm_role_definition" "rgscope" {
+  name        = "readaccess-uhn"
+  scope       = data.azurerm_resource_group.primary.id
+  description = "This is a custom role created via Terraform"
+
+  permissions {
+    actions     = ["read"]
+    not_actions = []
+  }
+
+  assignable_scopes = [
+    data.azurerm_resource_group.primary.id, # /subscriptions/00000000-0000-0000-0000-000000000000
+  ]
+}
