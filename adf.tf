e919937de14bafc6d5e0bd6952e62ad4d12e7ca3
Krishna, Neerati G <gopala.krishna@optum.com>
2023-03-05 17:10:52 +0000

    added adf

---
+++ adf.tf
@@ -0,0 +1,5 @@
+resource "azurerm_data_factory" "example" {
+  name                = "adf-ngkadf-001"
+  location            = azurerm_resource_group.githubactions.location
+  resource_group_name = azurerm_resource_group.githubactions.name
+}
