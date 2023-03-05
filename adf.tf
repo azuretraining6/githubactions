bcc0dea1c6125b20ed15507acd9429120796ee50
Krishna, Neerati G <gopala.krishna@optum.com>
2023-03-05 17:28:14 +0000

    added managed identity

--- adf.tf
+++ adf.tf
@@ -2,4 +2,7 @@ resource "azurerm_data_factory" "example" {
   name                = "adf-ngkadf-001"
   location            = azurerm_resource_group.githubactions.location
   resource_group_name = azurerm_resource_group.githubactions.name
+  identity {
+    type = "SystemAssigned"
+  }
 }
