2f7a477d6aed9f02445f0f42c73af4ec0276a9fa
Krishna, Neerati G <gopala.krishna@optum.com>
2023-03-07 22:28:08 +0000

    added new sp to datasource

--- data-sources.tf
+++ data-sources.tf
@@ -2,5 +2,5 @@ data "azuread_service_principal" "databrickssp" {
   application_id = "c740cd4a-e0b8-4299-84ee-531c9477fe2b"
 }
 output "dbssp" {
-        value = "${data.azurerm_azuread_service_principal.databrickssp.id}"
+        value = "${data.azuread_service_principal.databrickssp.id}"
 }
