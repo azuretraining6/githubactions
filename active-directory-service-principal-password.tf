68da4b3214df3bddfba50030144206217112c649
Krishna, Neerati G <gopala.krishna@optum.com>
2023-03-07 22:06:39 +0000

    added new SP

--- active-directory-service-principal-password.tf
+++ active-directory-service-principal-password.tf
@@ -1,4 +1,4 @@
-# module "service_principal_password_01" {
-#   source               = "git::https://github.com/uhg-internal/azuread_service_principal_password.git?ref=v0.0.1"
-#   service_principal_id = module.azuread_service_principal_01.output.id
-# }
+module "service_principal_password_01" {
+  source               = "git::https://github.com/uhg-internal/azuread_service_principal_password.git?ref=v0.0.1"
+  service_principal_id = module.azuread_service_principal_01.output.id
+}
