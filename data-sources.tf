859de54bf7686e04f0446cca26b911135e21f376
Krishna, Neerati G <gopala.krishna@optum.com>
2023-03-08 21:16:13 +0000

    only to run pipeline

--- data-sources.tf
+++ data-sources.tf
@@ -2,3 +2,6 @@ data "azuread_service_principal" "databrickssp" {
   display_name = "sp-testing"
   object_id = "2000a396-24be-420f-905e-420884391b1e"
 }
+output "dbssp" {
+        value = "${data.azuread_service_principal.databrickssp.id}"
+}
