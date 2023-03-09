7f9b8d0b1002b703509b0c50386dc71698e8e414
Krishna, Neerati G <gopala.krishna@optum.com>
2023-03-09 12:07:16 +0000

    static sp assign to role

--- data-sources.tf
+++ data-sources.tf
@@ -1,6 +1,6 @@
-data "azuread_service_principal" "databrickssp" {
-  display_name = "sp-testing"
-}
 data "azuread_application" "databrickssp" {
   display_name = "sp-testing"
+}
+data "azuread_service_principal" "databrickssp" {
+ application_id = data.azuread_application.databrickssp.application_id
 }
