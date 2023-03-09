339fe3afa8cf6ab6e6198f9544ba93c74baa54db
Krishna, Neerati G <gopala.krishna@optum.com>
2023-03-09 21:45:55 +0000

    destroy

--- role.tf
+++ role.tf
@@ -32,8 +32,8 @@ resource "azurerm_role_assignment" "example2" {
   principal_id       = azurerm_data_factory.example.identity[0].principal_id
 }
 
-resource "azurerm_role_assignment" "example3" {
-  scope              = azurerm_resource_group.githubactions.id
-  role_definition_id = azurerm_role_definition.rgscope.role_definition_resource_id
-  principal_id       = module.azuread_service_principal_01.output.id
-}
+# resource "azurerm_role_assignment" "example3" {
+#   scope              = azurerm_resource_group.githubactions.id
+#   role_definition_id = azurerm_role_definition.rgscope.role_definition_resource_id
+#   principal_id       = data.azuread_service_principal.databrickssp.id
+# }
