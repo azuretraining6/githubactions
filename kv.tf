013c34be334ff6af388c141956b1c840641a27f1
Krishna, Neerati G <gopala.krishna@optum.com>
2023-03-13 19:48:33 +0000

    added the KV

---
+++ kv.tf
@@ -0,0 +1,33 @@
+resource "azurerm_key_vault" "kv1" {
+  name                       = "ngktestkv451701"
+  location                   = azurerm_resource_group.githubactions.location
+  resource_group_name        = azurerm_resource_group.githubactions.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "premium"
+  soft_delete_retention_days = 7
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Create",
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Set",
+      "Get",
+      "Delete",
+      "Purge",
+      "Recover"
+    ]
+  }
+}
+
+
+resource "azurerm_key_vault_secret" "secret1" {
+  name         = "secret-sauce"
+  value        = azuread_application_password.appsecret.value
+  key_vault_id = azurerm_key_vault.kv1.id
+}
