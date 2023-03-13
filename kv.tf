6f089757ab9b9ebecb0e3a8810ba777d4b864cb9
Krishna, Neerati G <gopala.krishna@optum.com>
2023-03-13 20:50:06 +0000

    added other secrets

--- kv.tf
+++ kv.tf
@@ -25,6 +25,20 @@ resource "azurerm_key_vault" "kv1" {
   }
 }
 
+resource "azurerm_key_vault_access_policy" "kvpolicy1" {
+  key_vault_id = azurerm_key_vault.kv1.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = "cc965947-26a5-4211-aca0-f4351dc72bf7"
+
+
+  key_permissions = [
+    "Get",
+  ]
+
+  secret_permissions = [
+    "Get",
+  ]
+}
 
 resource "azurerm_key_vault_secret" "secret1" {
   name         = "secret-sauce"
@@ -32,8 +46,20 @@ resource "azurerm_key_vault_secret" "secret1" {
   key_vault_id = azurerm_key_vault.kv1.id
 }
 
-resource "azurerm_key_vault_secret" "subid" {
-  name         = "databricks-dev-ops-subscription-id"
+resource "azurerm_key_vault_secret" "clientid" {
+  name         = "databricks-dev-ops-client-id"
   value        = azuread_application.appreg.application_id
+  key_vault_id = azurerm_key_vault.kv1.id
+}
+
+resource "azurerm_key_vault_secret" "subid" {
+  name         = "subscriptionid"
+  value        = data.azurerm_client_config.current.subscription_id
+  key_vault_id = azurerm_key_vault.kv1.id
+}
+
+resource "azurerm_key_vault_secret" "tenantid" {
+  name         = "tenantid"
+  value        = data.azurerm_client_config.current.tenant_id
   key_vault_id = azurerm_key_vault.kv1.id
 }
