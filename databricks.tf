69ba89b9480b277a46af9fbce5f70e14fef2bd5e
Krishna, Neerati G <gopala.krishna@optum.com>
2023-03-14 22:27:00 +0000

    added the SP to workspace

--- databricks.tf
+++ databricks.tf
@@ -10,33 +10,10 @@ resource "azurerm_databricks_workspace" "dbs1" {
     Environment = "dev"
   }
 }
-
-
-# resource "azurerm_databricks_workspace" "dbs2" {
-#   name                = "ngkdatabrickstest22"
-#   resource_group_name = azurerm_resource_group.githubactions.name
-#   location            = azurerm_resource_group.githubactions.location
-#   sku                 = "premium"
-
-#   tags = {
-#     Environment = "dev"
-#   }
-# }
-# provider "databricks" {
-#   azure_workspace_resource_id = azurerm_databricks_workspace.dbs1.id
-# }
-# resource "databricks_workspace_conf" "new" {
-#   custom_config = {
-#     "enableIpAccessLists": true
-#   }
-# }
-# resource "databricks_ip_access_list" "allowed-list" {
-#   label     = "optumips"
-#   list_type = "ALLOW"
-#   ip_addresses = [
-#     "1.2.3.0/24",
-#     "1.2.5.0/24",
-#     "0.0.0.0/0"
-#   ]
-#   depends_on = [databricks_workspace_conf.new]
-# }
+provider "databricks" {
+  host = azurerm_databricks_workspace.dbs1.workspace_url
+}
+resource "databricks_service_principal" "dbs-sp" {
+  application_id = azuread_application.appreg.application_id
+    display_name   = azuread_application.appreg.display_name
+}
