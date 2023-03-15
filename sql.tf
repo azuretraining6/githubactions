458a9d9f5f7b7708d1485e27f7ddde19819b4300
Krishna, Neerati G <gopala.krishna@optum.com>
2023-03-15 12:17:11 +0000

    changed the db size

--- sql.tf
+++ sql.tf
@@ -22,10 +22,10 @@ resource "azurerm_mssql_database" "sqldb" {
   server_id      = azurerm_mssql_server.sql1.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
   license_type   = "LicenseIncluded"
-  max_size_gb    = 4
+  max_size_gb    = 1
   read_scale     = true
   sku_name       = "S0"
-  zone_redundant = true
+  zone_redundant = false
 
   tags = {
     foo = "bar"
