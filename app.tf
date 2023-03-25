5efeab839932a4c7c2879a29d13f7aea3eff2b3c
Krishna, Neerati G <gopala.krishna@optum.com>
2023-03-25 05:52:58 +0000

    added auth to the webapp

--- app.tf
+++ app.tf
@@ -11,6 +11,17 @@ resource "azurerm_windows_web_app" "webapp1" {
   resource_group_name = azurerm_resource_group.githubactions.name
   location            = azurerm_resource_group.githubactions.location
   service_plan_id     = azurerm_service_plan.plan1.id
-
+  identity {
+    type = "ManagedServiceIdentityTypeSystemAssigned"
+  }
   site_config {}
+  auth_settings {
+    enabled = true
+    unauthenticated_client_action = "AllowAnonymous"
+    active_directory {
+      client_id = "8ad8b609-53d1-4fde-9cf2-c8cd01b30906"
+    }
+    default_provider = "BuiltInAuthenticationProviderAzureActiveDirectory"
+
+  }
 }
