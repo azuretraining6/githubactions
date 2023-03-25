15fae9daafed2701007501210139175f8a2dc970
Krishna, Neerati G <gopala.krishna@optum.com>
2023-03-25 05:02:10 +0000

    added the webapp

---
+++ app.tf
@@ -0,0 +1,16 @@
+resource "azurerm_service_plan" "plan1" {
+  name                = "ngkauthtestwebplan01"
+  resource_group_name = azurerm_resource_group.githubactions.name
+  location            = azurerm_resource_group.githubactions.location
+  sku_name            = "P1v2"
+  os_type             = "Windows"
+}
+
+resource "azurerm_windows_web_app" "webapp1" {
+  name                = "ngkauthtestwebapp01"
+  resource_group_name = azurerm_resource_group.githubactions.name
+  location            = azurerm_resource_group.githubactions.location
+  service_plan_id     = azurerm_service_plan.plan1.id
+
+  site_config {}
+}
