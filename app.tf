9344f97262cac7ab54aea7f6f20a1a7d5b9b1092
Krishna, Neerati G <gopala.krishna@optum.com>
2023-03-28 21:49:23 +0100

    new app service

--- app.tf
+++ app.tf
@@ -1,5 +1,5 @@
 resource "azurerm_service_plan" "plan1" {
-  name                = "ngkauthtestwebplan01"
+  name                = "ngkauthtestwebplan02"
   resource_group_name = azurerm_resource_group.githubactions.name
   location            = azurerm_resource_group.githubactions.location
   sku_name            = "B1"
@@ -7,7 +7,7 @@ resource "azurerm_service_plan" "plan1" {
 }
 
 resource "azurerm_windows_web_app" "webapp1" {
-  name                = "ngkauthtestwebapp01"
+  name                = "ngkauthtestwebapp02"
   resource_group_name = azurerm_resource_group.githubactions.name
   location            = azurerm_resource_group.githubactions.location
   service_plan_id     = azurerm_service_plan.plan1.id
