3cc036fbbc661da0a86c91812ceb33e8f728d4d4
Krishna, Neerati G <gopala.krishna@optum.com>
2023-03-31 19:42:09 +0100

    added log policy

--- diag.tf
+++ diag.tf
@@ -1,22 +1,45 @@
-resource "azurerm_monitor_diagnostic_setting" "example" {
-  name               = "example"
-  target_resource_id = azurerm_managed_disk.example.id
-  eventhub_name = azurerm_eventhub.example.name
-  eventhub_authorization_rule_id = azurerm_eventhub_namespace_authorization_rule.disk.id
+# resource "azurerm_monitor_diagnostic_setting" "example" {
+#   name               = "example"
+#   target_resource_id = azurerm_managed_disk.example.id
+#   eventhub_name = azurerm_eventhub.example.name
+#   eventhub_authorization_rule_id = azurerm_eventhub_namespace_authorization_rule.disk.id
 
-  enabled_log {
-    category = "AuditEvent"
+#   enabled_log {
+#     category = "AuditEvent"
 
-    retention_policy {
-      enabled = false
-    }
-  }
+#     retention_policy {
+#       enabled = false
+#     }
+#   }
 
-  metric {
-    category = "AllMetrics"
+#   metric {
+#     category = "AllMetrics"
 
-    retention_policy {
-      enabled = false
-    }
+#     retention_policy {
+#       enabled = false
+#     }
+#   }
+# }
+resource "azurerm_monitor_log_profile" "example" {
+  name = "ngklogprofile"
+
+  categories = [
+    "Action",
+    "Delete",
+    "Write",
+  ]
+
+  locations = [
+    "westus",
+    "global",
+  ]
+
+  # RootManageSharedAccessKey is created by default with listen, send, manage permissions
+  servicebus_rule_id = "${azurerm_eventhub_namespace.example.id}/authorizationrules/RootManageSharedAccessKey"
+  
+
+  retention_policy {
+    enabled = true
+    days    = 7
   }
 }
