67a671d87da2a82e286678c965ba22ef421f1b8b
Krishna, Neerati G <gopala.krishna@optum.com>
2023-04-03 17:11:12 +0100

    added diag setting again

--- diag.tf
+++ diag.tf
@@ -1,61 +1,38 @@
-# resource "azurerm_monitor_diagnostic_setting" "example" {
-#   name               = "example"
-#   target_resource_id = azurerm_managed_disk.example.id
-#   eventhub_name = azurerm_eventhub.example.name
-#   eventhub_authorization_rule_id = azurerm_eventhub_namespace_authorization_rule.disk.id
+resource "azurerm_monitor_diagnostic_setting" "example" {
+  name               = "example"
+  target_resource_id = azurerm_managed_disk.example.id
+  eventhub_name = azurerm_eventhub.example.name
+  eventhub_authorization_rule_id = azurerm_eventhub_namespace_authorization_rule.disk.id
 
-#   enabled_log {
-#     category = "AuditEvent"
+  enabled_log {
+    category = "AuditEvent"
 
-#     retention_policy {
-#       enabled = false
-#     }
-#   }
+    retention_policy {
+      enabled = false
+    }
+  }
 
-#   metric {
-#     category = "AllMetrics"
+  metric {
+    category = "AllMetrics"
 
-#     retention_policy {
-#       enabled = false
-#     }
-#   }
-# }
-resource "azurerm_monitor_log_profile" "example" {
-  name = "ngklogprofile"
-
-  categories = [
-    "Action",
-    "Delete",
-    "Write",
-  ]
-
-  locations = [
-    "eastus",
-    "global",
-  ]
-
-  # RootManageSharedAccessKey is created by default with listen, send, manage permissions
-  servicebus_rule_id = "${azurerm_eventhub_namespace.example.id}/authorizationrules/RootManageSharedAccessKey"
-  
-
-  retention_policy {
-    enabled = true
-    days    = 7
+    retention_policy {
+      enabled = false
+    }
   }
 }
 
-# resource "azurerm_monitor_diagnostic_setting" "example" {
-#     name                           = "example"
-#     target_resource_id             = "/subscriptions/39215d40-4caf-49ad-a97f-7dab6ddfe062/resourceGroups/Infracode/providers/Microsoft.KeyVault/vaults/ngknewkvforpltest01"
+resource "azurerm_monitor_diagnostic_setting" "example" {
+    name                           = "example"
+    target_resource_id             = "/subscriptions/39215d40-4caf-49ad-a97f-7dab6ddfe062/resourceGroups/Infracode/providers/Microsoft.KeyVault/vaults/ngknewkvforpltest01"
 
-#     eventhub_name = azurerm_eventhub.example.name
-#     eventhub_authorization_rule_id = azurerm_eventhub_namespace_authorization_rule.example.id
+    eventhub_name = azurerm_eventhub.example.name
+    eventhub_authorization_rule_id = azurerm_eventhub_namespace_authorization_rule.example.id
 
-#     enabled_log {
-#         category = "Administrative"
+    enabled_log {
+        category = "Administrative"
 
-#         retention_policy {
-#         enabled = false
-#     }
-# }
-# }
+        retention_policy {
+        enabled = false
+    }
+}
+}
