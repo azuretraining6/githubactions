4cc7540498502cf129755afa4f68b915a9e441ca
Krishna, Neerati G <gopala.krishna@optum.com>
2023-04-12 18:22:23 +0100

    changed the files

--- ignorelist/kv.tf
+++ ignorelist/kv.tf
@@ -1,74 +1,41 @@
-resource "azurerm_key_vault" "kv1" {
-  name                       = "ngktestkv451702"
-  location                   = azurerm_resource_group.githubactions.location
-  resource_group_name        = azurerm_resource_group.githubactions.name
-  tenant_id                  = data.azurerm_client_config.current.tenant_id
-  sku_name                   = "premium"
-  soft_delete_retention_days = 7
+# resource "azurerm_key_vault" "ngkvault" {
+#   name                        = "ngksqlpasswdkv001"
+#   location                    = azurerm_resource_group.githubactions.location
+#   resource_group_name         = azurerm_resource_group.githubactions.name
+#   enabled_for_disk_encryption = true
+#   tenant_id                   = data.azurerm_client_config.current.tenant_id
+#   soft_delete_retention_days  = 7
+#   purge_protection_enabled    = false
 
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
+#   sku_name = "standard"
 
-    key_permissions = [
-      "Create",
-      "Get",
-    ]
+#   access_policy {
+#     tenant_id = data.azurerm_client_config.current.tenant_id
+#     object_id = data.azurerm_client_config.current.object_id
 
-    secret_permissions = [
-      "Set",
-      "Get",
-      "Delete",
-      "Purge",
-      "Recover"
-    ]
-  }
-}
+#     key_permissions = [
+#       "Get",
+#     ]
 
-resource "azurerm_key_vault_access_policy" "kvpolicy1" {
-  key_vault_id = azurerm_key_vault.kv1.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = "cc965947-26a5-4211-aca0-f4351dc72bf7"
+#     secret_permissions = [
+#       "Get",
+#       "List",
+#       "Set",
+#       "Get",
+#       "Delete",
+#       "Purge",
+#       "Recover"
 
+#     ]
 
-  key_permissions = [
-    "Get",
-    "List",
-  ]
+#     storage_permissions = [
+#       "Get",
+#     ]
+#   }
+# }
 
-  secret_permissions = [
-    "Get",
-    "List",
-  ]
-}
-
-resource "azurerm_key_vault_secret" "secret1" {
-  name         = "secret-sauce"
-  value        = azuread_application_password.appsecret.value
-  key_vault_id = azurerm_key_vault.kv1.id
-}
-
-resource "azurerm_key_vault_secret" "clientid" {
-  name         = "databricks-dev-ops-client-id"
-  value        = azuread_application.appreg.application_id
-  key_vault_id = azurerm_key_vault.kv1.id
-}
-
-resource "azurerm_key_vault_secret" "subid2" {
-  name         = "subscriptionid2"
-  value        = data.azurerm_client_config.current.subscription_id
-  key_vault_id = azurerm_key_vault.kv1.id
-}
-
-resource "azurerm_key_vault_secret" "tenantid" {
-  name         = "tenantid"
-  value        = data.azurerm_client_config.current.tenant_id
-  key_vault_id = azurerm_key_vault.kv1.id
-}
-
-
-resource "azurerm_key_vault_secret" "sqlpasswd" {
-  name         = "sqlpasswd"
-  value        = random_password.sqlpasswd.result
-  key_vault_id = azurerm_key_vault.kv1.id
-}
+# resource "azurerm_key_vault_secret" "sqladminpassword" {
+#   name         = "sqlpasswd"
+#   value        =  random_password.password.result
+#   key_vault_id = azurerm_key_vault.ngkvault.id
+# }
