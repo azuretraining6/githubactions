0575fb32782c6deabd658ef709ce966577402837
Krishna, Neerati G <gopala.krishna@optum.com>
2023-04-12 19:21:54 +0100

    added another storage account

--- vm.tf
+++ vm.tf
@@ -1,4 +1,14 @@
+resource "azurerm_storage_account" "example" {
+  name                     = "ngkstordiag001"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
 
+  tags = {
+    environment = "staging"
+  }
+}
 
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
