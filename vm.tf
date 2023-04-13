c2a334ed4f75e2acea21281757c305cdf5fff215
Krishna, Neerati G <gopala.krishna@optum.com>
2023-04-14 00:19:57 +0100

    added identity

--- vm.tf
+++ vm.tf
@@ -48,6 +48,9 @@ resource "azurerm_windows_virtual_machine" "example" {
   size                = "Standard_F2"
   admin_username      = "adminuser"
   admin_password      = "P@$$w0rd1234!"
+  identity {
+    type = "SystemAssigned"
+  }
   network_interface_ids = [
     azurerm_network_interface.example.id,
   ]
